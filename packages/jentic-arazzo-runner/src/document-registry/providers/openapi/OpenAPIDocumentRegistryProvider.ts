5d6d1713f8f68cee5b389f98b17598b69f52ab4d
Vladimir Gorej <vladimir.gorej@gmail.com>
2026-03-11 21:32:38 +0100

    fix: mutable mode

--- packages/jentic-arazzo-runner/src/document-registry/providers/openapi/OpenAPIDocumentRegistryProvider.ts
+++ packages/jentic-arazzo-runner/src/document-registry/providers/openapi/OpenAPIDocumentRegistryProvider.ts
@@ -58,6 +58,7 @@ const defaultOptions: OpenAPIDocumentRegistryProviderOptions = {
   },
   dereference: {
     strategies: resolverDefaultOptions.dereference!.strategies,
+    immutable: false,
   },
 };
 
